12c8d7632d748bca741d71d36b316405067b56b3
Someone <rgbjkbs@hdbf.ru>
2018-12-14 16:47:40 +0200

    view render

--- vendor/core/View.php
+++ vendor/core/View.php
@@ -1,16 +1,8 @@
 <?php
 namespace core;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of View
- *
- * @author web
- */
 class View {
-    //put your code here
+    public function render(){
+        include_once 'vendor/views/templates/main.php';
+    }
 }
