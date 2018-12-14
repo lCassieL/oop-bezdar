70ab4f2d280167f6dcae8efff24dfc4d425f78c8
Someone <rgbjkbs@hdbf.ru>
2018-12-14 16:39:25 +0200

    autoloader

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 
+spl_autoload_register(function($class){
+    $file='vendor'.DIRECTORY_SEPARATOR.$class.'.php';
+    if(file_exists($file)){
+        include_once $file;
+        return true;
+    }
+    return false;
+});
