1d6b6942731df164c6346c6cd8b0afccb547bbab
Someone <rgbjkbs@hdbf.ru>
2018-12-14 16:52:19 +0200

    model base

--- vendor/core/models/Main.php
+++ vendor/core/models/Main.php
@@ -1,16 +1,6 @@
 <?php
 namespace core\models;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of Main
- *
- * @author web
- */
 class Main {
-    //put your code here
+    
 }
